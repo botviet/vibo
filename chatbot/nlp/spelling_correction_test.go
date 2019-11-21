68a2bc176446e9fc1089424134e665319424b94a
nghia <trantrungnghia.ptit@gmail.com>
2019-11-22 00:41:27 +0900

    update

--- chatbot/nlp/spelling_correction_test.go
+++ chatbot/nlp/spelling_correction_test.go
@@ -17,7 +17,7 @@ func TestCorrection(t *testing.T) {
 	assert.Equal(t, "ăn", dic.Correction("an", []string{"cơm"}, []string{"đi"}))
 	assert.Equal(t, "ơi", dic.Correction("oi", []string{"bạn"}, []string{}))
 	assert.Equal(t, "nghiêng", dic.Correction("nghienga", []string{"ngả"}, []string{"đi"}))
-	assert.Equal(t, "chán", dic.Correction("chan", []string{"quá", "đi"}, []string{"haizz"}))
+	assert.Equal(t, "chán", dic.Correction("chan", []string{"quá"}, []string{}))
 	assert.Equal(t, "đi", dic.Correction("di", []string{"chơi", "không"}, []string{"ê"}))
 	assert.Equal(t, "gì", dic.Correction("gì", []string{"là", "tên", "bạn"}, []string{}))
 }
